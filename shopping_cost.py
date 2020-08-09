f0337f5a24a852fab61ba535258fb9451163cb25
DISHI BHATT <51319282+dishibhatt05@users.noreply.github.com>
2020-08-09 13:23:01 +0530

    Add files via upload

---
+++ shopping_cost.py
@@ -0,0 +1,12 @@
+'''
+Shopping Cost
+Tina is preparing a shopping list containing N items. She knows the quantity and price of each item on the list. 
+The shop is offering a 20% discount on the items with a quantity of more than 100 units. 
+Given the quantity and price of each of the N items, your task is to calculate how much each item will cost.
+ '''
+n=int(input())
+for i in range(n):
+  x, y = [int(x) for x in input().split()]  
+  amt=x*y
+  if x>=100:
+    print(x-(0.2*x))
